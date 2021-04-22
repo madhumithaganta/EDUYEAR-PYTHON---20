726d232a6c8ca9c98d7976058ec794968f00a196
madhumithaganta <82998722+madhumithaganta@users.noreply.github.com>
2021-04-22 13:33:52 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,10 @@
+employee='srirag'
+id=101
+salary=6.5
+company='rajput industries'
+male=True
+print("iam srirag from rajput industries and my income is 6.5.")
+print("iam",employee,"from",company,"and my income is",salary,)
+print("iam {} from {} and my income is {}.".format(employee, company, salary))
+
+
