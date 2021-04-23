56a6d719f88b81fafb0a111d5e61287aee348539
madhumithaganta <82998722+madhumithaganta@users.noreply.github.com>
2021-04-23 14:26:18 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,16 @@
+birth_year=1987
+end_year=2021
+age=birth_year-end_year
+print(age)
+a,b=15,6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a**b)
+string='why are you yeling at him daily'
+print(string)
+print('count occurance of y in the string{}'.format(string.count('y')))
+a='she is pathetic'
+print(a[1:9:1])
+
